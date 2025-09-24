89e7f60c07c1bd04d5203a4bd33dc12d2501454d
salperezate <salperezate@unal.edu.co>
2025-09-24 06:39:23 -0500

    Addition of MaxSigmoidAttnBlock in the nn import for 2nd strategy

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -71,6 +71,7 @@ from ultralytics.nn.modules import (
     CBAM,
     ChannelAttention,
     SpatialAttention,
+    MaxSigmoidAttnBlock,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, YAML, colorstr, emojis
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
